6b599d647b5c692a4fb043f9f55643e104485d7c
catsimple <catsimple@qq.com>
2024-02-04 20:58:26 +0800

    Update leigod_signin.js

--- leigod_signin.js
+++ leigod_signin.js
@@ -2,16 +2,18 @@
 //LEISHEN_REPEAT_NOTIFY：是否开启重复暂停通知，默认为false，只有在当前账号未暂停时，首次暂停才会推送通知。将其设置为true则不管当前账号是否已经在任务执行前暂停，开启后即每次执行任务都会推送通知.
 // 设置想要输出的时间点，这样只有在指定时间会推送通知，其余时间不会通知
 const targetHour = 09;
-const targetMinute = 20;
-cron: 10 * * * *
+//const targetMinute = 20;
+
 const $ = new Env('雷神加速器自动暂停');
+cron: 20 */1 * * *
+
 const notify = $.isNode() ? require('./sendNotify') : '';
 const fetch = require('node-fetch');
 const md5 = require('js-md5');
 const { exec } = require('child_process');
 const now = new Date();
 const currentHour = now.getHours();
-const currentMinute = now.getMinutes();
+//const currentMinute = now.getMinutes();
 
 
 
